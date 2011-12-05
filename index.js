61c6acea797e7f8386bd14fb77bdf9db34ed849f
James Halliday <mail@substack.net>
2011-12-05 15:03:09 -0800

    stack test passes

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-module.exports = function (dir, cb) {
-    var cwd = process.cwd();
+module.exports = chdir;
+
+function chdir (dir, cb) {
+    chdir.push();
     process.chdir(dir);
     cb();
-    process.chdir(cwd);
+    chdir.pop();
+};
+chdir.stack = [];
+
+chdir.push = function (dir) {
+    chdir.stack.push(dir === undefined ? process.cwd() : dir);
+};
+
+chdir.pop = function () {
+    var dir = chdir.stack.shift();
+    if (dir !== undefined) process.chdir(dir);
 };
